2bc119f37e57a82bc0d57575509ee854885db312
Zozo <gulabsinghrajput008@gmail.com>
2025-07-15 21:57:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# Start aria2c daemon for aria2p (runs in background with RPC)
-aria2c --daemon=true --enable-rpc --rpc-listen-all=true --rpc-allow-origin-all --rpc-listen-port=6800
+# Start aria2c daemon
+aria2c --enable-rpc --rpc-listen-all=true --rpc-allow-origin-all --rpc-listen-port=6800 --daemon=true
 
-# Apply file descriptor limits
-ulimit -n 100000
-
-# Apply TCP tuning (if supported)
-sysctl -p /etc/sysctl.conf
-
-# Run the bot
+# Start the bot
 python bot.py
