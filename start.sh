95f1ebb513ae9e6d5c59378475a7c5daea85d316
Zozo <gulabsinghrajput008@gmail.com>
2025-07-15 15:38:23 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+# Start aria2c daemon for aria2p (runs in background with RPC)
 aria2c --daemon=true --enable-rpc --rpc-listen-all=true --rpc-allow-origin-all --rpc-listen-port=6800
 
 # Apply file descriptor limits
